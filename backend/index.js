eb3be8ba86fe4465e351ade78351a751e50a9f9c
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-01-28 22:54:02 +0530

    little update

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,4 @@ const mainRoute = require("./routes/index");
 const app = express();
 
 app.use("api/v1", mainRoute);
-app.listen(3000, () => console.log("server is running okay"));
+app.listen(3000);
